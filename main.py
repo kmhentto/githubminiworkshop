c58312f9196597daf70f8a4b31f24068bb39dde8
Satugr <159280076+Satugr@users.noreply.github.com>
2025-11-27 14:38:51 +0200

    Testipäivitys

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 # tee oma haara, sen jälkeen lisää nimesi printtaus alla olevaan tapaan, kommitoi muutokset ja luo uusi PR
-print("Katja editoi GitHubissa simppelisti")
-print("Katja editoi Codespaces -ympäristössä")
+print("Satu editoi GitHubissa simppelisti")
+print("Satu editoi Codespaces -ympäristössä")
