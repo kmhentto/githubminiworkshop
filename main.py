b764350bfd1acdd315bece21ac363caab94e4d87
Petri Lehto <PEL00030@LAUREA.FI>
2025-11-27 14:38:03 +0200

    Lisäsin pari tekstiriviä

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # tee oma haara, sen jälkeen lisää nimesi printtaus alla olevaan tapaan, kommitoi muutokset ja luo uusi PR
 print("Katja editoi GitHubissa simppelisti")
 print("Katja editoi Codespaces -ympäristössä")
+print("Petrismi editoi tätä")
+print("ja tätä")
