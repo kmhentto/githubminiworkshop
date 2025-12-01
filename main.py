729dcd43fb25ff04b7df90ceff95cb94ea8157d2
FEL0001 <FEL00001@laurea.fi>
2025-12-01 15:41:39 +0200

    Lisäsin nimeni

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # tee oma haara, sen jälkeen lisää nimesi printtaus alla olevaan tapaan, kommitoi muutokset ja luo uusi PR
 print("Katja editoi GitHubissa simppelisti!!!")
 print("Katja editoi Codespaces -ympäristössä")
+print("Felipe!")
 
 
 
